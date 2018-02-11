44dda9c43d919c837a12339df12caf14bb4fc63e
Shehroze Bhatti <shehrozebhatti@Shehrozes-MacBook-Pro-200.local>
2018-02-11 15:52:32 +0500

    new changes

--- main_training_kaggle.py
+++ main_training_kaggle.py
@@ -236,6 +236,8 @@ def start_threads(args):
          
         # encode training data set
         Xtrain = tokenizer.texts_to_matrix(docs, mode='freq')
+        ytrain = array([0 for _ in range(900)] + [1 for _ in range(900)])
+
         print(Xtrain.shape)
          
         # load all test reviews
@@ -244,7 +246,22 @@ def start_threads(args):
         docs = negative_lines + positive_lines
         # encode training data set
         Xtest = tokenizer.texts_to_matrix(docs, mode='freq')
+        ytest = array([0 for _ in range(100)] + [1 for _ in range(100)])
+
         print(Xtest.shape)
+
+        n_words = Xtest.shape[1]
+        # define network
+        model = Sequential()
+        model.add(Dense(50, input_shape=(n_words,), activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
+        # compile network
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        # fit network
+        model.fit(Xtrain, ytrain, epochs=50, verbose=2)
+        # evaluate
+        loss, acc = model.evaluate(Xtest, ytest, verbose=0)
+        print('Test Accuracy: %f' % (acc*100))
 
     else:
 
